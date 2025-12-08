f1e92639226245a1ffd8650a4cbab531b4ef8bc9
Christopher Yousufzad <christopher.y@gmx.de>
2025-12-08 12:36:05 +0100

    revert

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
 	return (
 		<header className="App-header" style={{padding:20}}>
-			<h1 style={{margin:0}}>CMS_gruppenarbeit TEST1Learh4ri4hh234443</h1>
+			<h1 style={{margin:0}}>CMS_gruppenarbeit TestS</h1>
 			<nav style={{marginTop:12}}>
 				<a href="#home" style={{color:'#61dafb',marginRight:12}}>Home</a>
 				<a href="#about" style={{color:'#fff',opacity:0.85}}>About</a>
