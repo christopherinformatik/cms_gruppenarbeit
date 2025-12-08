4c0f1f33d8b98ff21b5dc3358189c80709c0dc1c
Christopher Yousufzad <christopher.y@gmx.de>
2025-12-08 13:09:54 +0100

    mui installation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
 import Home from './components/Home';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />Test
+      <ResponsiveAppBar />
       <Home />
       <Footer />
     </div>
