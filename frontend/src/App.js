e6d3cd44c9fdeda30af8b2d06d2349b13564b159
Christopher Yousufzad <christopher.y@gmx.de>
2025-12-08 11:26:49 +0100

    automatisches laden der Seite

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header />Test
       <Home />
       <Footer />
     </div>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
 	return (
 		<header className="App-header" style={{padding:20}}>
-			<h1 style={{margin:0}}>cms_gruppenarbeit</h1>
+			<h1 style={{margin:0}}>CMS_gruppenarbeit TEST123</h1>
 			<nav style={{marginTop:12}}>
 				<a href="#home" style={{color:'#61dafb',marginRight:12}}>Home</a>
 				<a href="#about" style={{color:'#fff',opacity:0.85}}>About</a>
